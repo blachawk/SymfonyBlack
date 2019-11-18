56cc1ebbc3aaf45c69af4dc781a2d3b7c9ab804c
Kensley Lewis <klpsupport@gmail.com>
2019-11-18 16:35:22 -0500

    - proper way on how to import js and css into our twig projects.

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,12 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+import '../css/app.css';
+
+import getNiceMessage from './get_nice_message';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+console.log(getNiceMessage(5));
