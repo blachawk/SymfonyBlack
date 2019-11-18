1409a360fb93154ca7174d756bc9fb3aaf87804a
Kensley Lewis <klpsupport@gmail.com>
2019-11-18 17:38:48 -0500

    How to properly load and work with sass

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-import '../css/app.css';
+import '../css/app.scss';
 
 import getNiceMessage from './get_nice_message';
 
