6ce83f6b5cb9e4320a102974527d2f9120c4bf37
Kensley Lewis <klpsupport@gmail.com>
2019-11-21 08:24:57 -0500

    - The copyFiles method is nice because it allows us to keep all our original images in the assets directory.
    - Enabled hash with images with the help of the examples provided in \webpack-encore\index.js file.

--- webpack.config.js
+++ webpack.config.js
@@ -57,6 +57,16 @@ Encore
     // enables Sass/SCSS support
     .enableSassLoader()
 
+
+    //copy files
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[hash:8].[ext]'
+    })
+   
+    
+
+
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
